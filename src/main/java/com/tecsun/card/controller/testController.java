ae38b8ce168b1de52ebda2029aad9190004dffc2
0214 <807500966@qq.com>
2018-09-07 00:28:41 +0800

    20180907 swagger2 构建成功

--- src/main/java/com/tecsun/card/controller/testController.java
+++ src/main/java/com/tecsun/card/controller/testController.java
@@ -1,18 +1,23 @@
 package com.tecsun.card.controller;
 
 import com.tecsun.card.entity.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping(value = "test")
+//@Controller
+@RestController
+@Api(description = "收货地址")
 public class testController {
     private static final Logger logger = LoggerFactory.getLogger(testController.class);
     @RequestMapping(value = "test", method = RequestMethod.GET)
+    @ApiOperation(value = "获得所有收货地址")
     @ResponseBody
     public Result test() {
         logger.info("请求成功：状态码{}", 200);
